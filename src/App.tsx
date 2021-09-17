5539ec183f259396fc2e55980ca58bc544755bd5
ysuzuki19 <ysuzuki.cpp@gmail.com>
2021-09-18 01:18:05 +0900

    move jest-haste-map to devDep

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
-const App:React.FC = () => {
-  return (
-    <>
-    </>
-  );
-}
+const App: React.FC = () => {
+  return <>Auto Deployment was completed.</>;
+};
 
 export default App;
